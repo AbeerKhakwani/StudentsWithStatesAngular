b817b898c77ed43b3db3d8f53724d845fdaac841
Jill Kuchman & Abeer Khakwani <student@epicodus.com>
2015-04-22 11:47:59 -0700

    Add UtilitiesFactory.js

--- app.js
+++ app.js
@@ -11,4 +11,11 @@ courseRoster.config(function($stateProvider) {
         templateUrl:"partials/courses.html",
         controller:'CoursesCtrl'
     });
+
+    $stateProvider.state('courses.students', {
+        url: "/:courseId",
+        templateUrl: "partials/courses.students.html",
+        controller: 'StudentsCtrl'
+    });
+    
 });
